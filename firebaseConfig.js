e96dafa6404c7b97bd7956197e2bddeb7a6b3f91
thangsianpau <prexpau123@gmail.com>
2025-05-03 15:16:08 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,21 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVguBnGKZv3MEsmsjS1IA6IYULTPUt-qk",
+  authDomain: "carbon-brain.firebaseapp.com",
+  projectId: "carbon-brain",
+  storageBucket: "carbon-brain.firebasestorage.app",
+  messagingSenderId: "119580069675",
+  appId: "1:119580069675:web:a91c06161ff8ecbfbaff6e",
+  measurementId: "G-T7F0KFC395
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { db };
 
