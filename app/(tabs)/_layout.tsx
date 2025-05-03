8bbfca964be1c65da7ef689e0af532e1b2924118
thangsianpau <prexpau123@gmail.com>
2025-05-03 16:20:55 -0500

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -43,7 +43,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="../progress" // ✅ this explicitly loads from app/progress.tsx
+        name="progress"
         options={{
           title: 'Progress',
           tabBarIcon: ({ color }) => (
